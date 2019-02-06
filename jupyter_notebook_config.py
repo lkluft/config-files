59343119df69c816547edf69410f085d18825e1b
Lukas Kluft <lukas.kluft@gmail.com>
2019-02-06 16:11:49 +0100

    Streamline jupyter notebook configuration
    Do not set a password hash. Since version 5.0 the password is stored in
    a separate ``jupyter_notebook_config.json`` file, which can be modified
    conveniently using ``jupyter notebook password``.
    
    Always listen on the same port on different machines. On the local
    machines different remote machines can be addressed using differing port
    forwarding.

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,43 +1,5 @@
-# -----------------------------------------------------------------------------
 # NotebookApp configuration
-# -----------------------------------------------------------------------------
 c = get_config()
 
-# Whether to open in a browser after starting. The specific browser used is
-# platform dependent and determined by the python standard library `webbrowser`
-# module, unless it is overridden using the --browser (NotebookApp.browser)
-# configuration option.
-c.NotebookApp.open_browser = False
-
-
-# The port the notebook server will listen on.
-def get_port():
-    from socket import gethostname
-    import re
-
-    # Maps regex to port number.
-    port_map = {
-        'thunder7': 4201,
-        'mlogin.*': 4202,
-        'mistralpp.*': 4203,
-    }
-
-    hostname = gethostname()
-    for pattern, value in port_map.items():
-        if re.compile(pattern).match(hostname):
-            # Return the corresponding port for first matching regex pattern.
-            return value
-    # Return default port if no match if found.
-    return 8888
-
-
-c.NotebookApp.port = get_port()
-
-# Hashed password to use for web authentication.
-#
-# To generate, type in a python/IPython shell:
-#
-#   from IPython.lib import passwd; passwd()
-#
-# The string should be of the form type:salt:hashed-password.
-c.NotebookApp.password = u'sha1:129681ef05fb:83a32ab1bd27080e880c6dfa3b7eb07a42338d4f'
+c.NotebookApp.open_browser = False  # Whether to open a browser on startup
+c.NotebookApp.port = 4242  # Port the notebook server will listen on
