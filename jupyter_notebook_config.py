b2e42ec04a4c66fa1d8fd1ccce61ba14157b1146
Lukas Kluft <lukas.kluft@gmail.com>
2019-08-06 09:17:07 +0200

    Enable matplotlib inline magic

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -3,3 +3,8 @@ c = get_config()
 
 c.NotebookApp.open_browser = False  # Whether to open a browser on startup
 c.NotebookApp.port = 4242  # Port the notebook server will listen on
+
+# IPython magic
+c.InteractiveShellApp.exec_lines = [
+    '%matplotlib inline',
+]
